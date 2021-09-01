db127415d2a1320eebafd2c41cab48edca57d5fb
nikolai <tevsnn2020@gmail.com>
2021-09-01 14:25:53 +0300

    Add 1 task

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+'use strict'
+
+function sumTo(n) {
+    if(n == 1) return 1;
+    return n + sumTo(n - 1)
+}
+
+console.log(sumTo(4))
+
+/**
+ * ==========================================================
+ */
+
